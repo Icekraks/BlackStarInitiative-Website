13b21fb6802b88456b1ecf3d9a2c3152961dcf8d
Felix-Hu-DotDev <felix@dotcommerce.com.au>
2024-09-27 14:39:21 +1000

    feature: padding adjustment

--- app/src/components/Sections/SocialSection/SocialSection.tsx
+++ app/src/components/Sections/SocialSection/SocialSection.tsx
@@ -55,8 +55,8 @@ const SocialSection: React.FC<SocialSectionProps> = ({
   return (
     <div
       className={cn(
-        "relative pb-12 lg:pb-16 2xl:pb-20 px-4 md:px-16",
-        sectionIndex !== 0 ? "pt-12 lg:pt-16 2xl:pt-20" : "",
+        "relative pb-12 lg:pb-16 px-4 md:px-16",
+        sectionIndex !== 0 ? "pt-12 lg:pt-16" : "",
         backgroundColour
       )}
     >
